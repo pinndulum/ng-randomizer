a284730d8fd4e014545fae033d8b2f6c9d638680
@pinndulum <jemecham@gmail.com>
2023-04-28 11:44:34 -0600

    query string for word banks

--- src/app/components/pages/buzz-words/buzz-words.component.ts
+++ src/app/components/pages/buzz-words/buzz-words.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject, withLatestFrom } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { MockService } from 'src/app/services/mock.service';
 
 @Component({
@@ -13,12 +16,31 @@ export class BuzzWordsComponent implements AfterViewInit {
   public readonly buzz_words$: Observable<string>;
   private readonly word_sub: Subject<any> = new Subject();
   
-  constructor(private mock: MockService) {
+  constructor(
+    private loc: Location,
+    private mock: MockService,
+    private route: ActivatedRoute
+  ) {
     this.buzz_words$ = this.word_sub.pipe();
   }
 
   ngAfterViewInit(): void {
-    this.load();
+    this.route.params.pipe(
+      withLatestFrom(this.route.queryParams),
+      tap(([parms, query]) => {
+        const [x, y, z] = [
+          query['x'], query['y'], query['z']
+        ];
+        const words = [
+          buzz_words[0][+x], buzz_words[1][+y], buzz_words[2][+z]
+        ].filter(x => !!x);
+        if (words.length === 3) {
+          this.word_sub.next(words.join(' '));
+        } else {
+          this.load();
+        }
+      })
+    ).subscribe();
   }
 
   load = () => {
@@ -28,6 +50,19 @@ export class BuzzWordsComponent implements AfterViewInit {
       this.mock.realistic.from(buzz_words[2])
     ];
     this.word_sub.next(words.join(' '));
+
+    const params = new URLSearchParams(location.search);
+    const [x, y, z] = words.map((x, i) =>
+      buzz_words[i].indexOf(x).toString()
+    );
+    params.set('x', x);
+    params.set('y', y);
+    params.set('z', z);
+    const search = params.toString();
+    if (search !== location.search) {
+      const state = this.loc.getState();
+      this.loc.replaceState('random/buzz-words', search, state);
+    }
   };
 }
 
--- src/app/components/pages/shakespeare-insults/shakespeare-insults.component.ts
+++ src/app/components/pages/shakespeare-insults/shakespeare-insults.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject, withLatestFrom } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { MockService } from 'src/app/services/mock.service';
 
 @Component({
@@ -13,12 +16,31 @@ export class ShakespeareInsultsComponent implements AfterViewInit {
   public readonly insults$: Observable<string>;
   private readonly word_sub: Subject<any> = new Subject();
   
-  constructor(private mock: MockService) {
+  constructor(
+    private loc: Location,
+    private mock: MockService,
+    private route: ActivatedRoute
+  ) {
     this.insults$ = this.word_sub.pipe();
   }
 
   ngAfterViewInit(): void {
-    this.load();
+    this.route.params.pipe(
+      withLatestFrom(this.route.queryParams),
+      tap(([parms, query]) => {
+        const [x, y, z] = [
+          query['x'], query['y'], query['z']
+        ];
+        const words = [
+          insults[0][+x], insults[1][+y], insults[2][+z]
+        ].filter(x => !!x);
+        if (words.length === 3) {
+          this.word_sub.next(words.join(' '));
+        } else {
+          this.load();
+        }
+      })
+    ).subscribe();
   }
 
   load = () => {
@@ -28,6 +50,19 @@ export class ShakespeareInsultsComponent implements AfterViewInit {
       this.mock.realistic.from(insults[2])
     ];
     this.word_sub.next(words.join(' '));
+    
+    const params = new URLSearchParams(location.search);
+    const [x, y, z] = words.map((x, i) =>
+      insults[i].indexOf(x).toString()
+    );
+    params.set('x', x);
+    params.set('y', y);
+    params.set('z', z);
+    const search = params.toString();
+    if (search !== location.search) {
+      const state = this.loc.getState();
+      this.loc.replaceState('random/shakespeare-insults', search, state);
+    }
   };
 }
 
